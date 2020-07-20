1846414caa5dcc12be4934a6b7917011a0b41656
ashad912 <ashad912@gmail.com>
2020-07-21 00:53:47 +0200

    Ticketing-app. Error handling completed

---
+++ ticketing-app/auth/src/errors/index.ts
@@ -0,0 +1,6 @@
+import { RequestValidationError } from './request-validation-error'
+import { DatabaseConnectionError } from './database-connection-error'
+import { CustomError } from './custom-error'
+import { NotFoundError } from './not-found-error'
+
+export { CustomError, RequestValidationError, DatabaseConnectionError, NotFoundError }
---
+++ ticketing-app/auth/src/routes/signup.ts
@@ -0,0 +1,46 @@
+import express, { Request, Response } from 'express'
+import { body, validationResult } from 'express-validator' // We need valdiate request body
+import { RequestValidationError, DatabaseConnectionError} from '../errors'
+// validationResult is used to pull out validation information
+// It appends some fields to req object
+
+const router = express.Router()
+
+router.post('/api/users/signup', [
+    body('email')
+        .isEmail()
+        .withMessage('Email must be valid'),
+    body('password')
+        .trim()
+        .isLength({ min: 4, max: 20 })
+        .withMessage('Password must be between 4 and 20 chars')
+],
+    // Typescript is angry, because req and res have no them types specifed.
+    // Need to import Request and Response types from express.
+    async (req: Request, res: Response) => {
+        const errors = validationResult(req) // Kinda Object
+
+        if(!errors.isEmpty()){
+            // Collecting error reasons in JS way...
+            // TS does not allow to attach non-existing prop to existing object!
+            // In TS we need to build subclass.
+            /*
+
+            const error = new Error('Invalid email or password')
+            error.reasons = errors.array() // Invoke errors method converting object to array
+            throw error
+            
+            */
+            
+            throw new RequestValidationError(errors.array())
+        }
+
+        const { email, password } = req.body
+
+        console.log('Creating a user...')
+
+        throw new DatabaseConnectionError()
+        res.send({})
+    })
+
+export { router as signupRouter }
