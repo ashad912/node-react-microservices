7aae70ddb9e8afe839fdd57fae111130977eb9a4
ashad912 <ashad912@gmail.com>
2021-04-27 18:27:01 +0200

    [vid-478/479] When Inference Does Not Work

---
+++ typescript/features/annotations/variables.ts
@@ -0,0 +1,58 @@
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+let now: Date = new Date();
+
+let colors: string[] = ['red', 'green', 'blue']
+let myNumbers: number[] = [1, 2, 3]
+let truths: boolean[] = [true, false, false]
+
+class Car {
+
+}
+
+let car: Car = new Car()
+
+
+let point: { x: number; y: number } = {
+    x: 10,
+    y: 20
+}
+
+const logNumber: (i: number) => void = (i: number) => {
+    console.log(i)
+}
+
+function log(i: number): void {
+    console.log(i)
+}
+
+
+// 1. "Any" type
+const json = '{"x":10, "y":20}'
+const coords: { x: number; y: number } = JSON.parse(json)
+
+console.log(coords.y)
+
+// 2. Later init 
+let words = ['red', 'green', 'blue']
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+    if (words[i] === 'green') {
+        foundWord = true
+    }
+}
+// 3. Cannot be inferred correctly
+
+let numbers = [-10, -1, 12]
+let numberAboveZero: boolean | number = false
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0)
+        numberAboveZero = numbers[i]
+}
--- typescript/fetchjson/index.ts
+++ typescript/fetchjson/index.ts
@@ -7,7 +7,27 @@ const url = 'https://jsonplaceholder.typicode.com/todos/1';
 //     console.log(res.data)
 // })()
 
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
 axios.get(url)
-    .then((res) => {
-        console.log(res.data)
+    .then((response) => {
+        const todo = response.data as Todo;
+
+        const id = todo.id
+        const title = todo.title
+        const completed = todo.completed
+        logTodo(id, title, completed)
+
     })
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+    console.log(`
+        The Todo with id: ${id}
+        Has a title of: ${title}
+        Is it finished? ${completed}
+    `)
+}
