6f56a0531f6be37715ad05f5051bc22873ad1548
ashad912 <ashad912@gmail.com>
2021-08-15 20:29:54 +0200

    [VID-195] COMPLETED Building a Next Image

--- ticketing-app/auth/src/test/setup.ts
+++ ticketing-app/auth/src/test/setup.ts
@@ -1,11 +1,16 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
+import request from 'supertest'
+
 import { app } from "../app"
 
 let mongo: any;
 
 beforeAll(async () => {
-    mongo = new MongoMemoryServer()
+
+    process.env.JWT_KEY = 'asdasd'
+
+    mongo = await MongoMemoryServer.create()
     const mongoUri = mongo.getUri()
 
     await mongoose.connect(mongoUri, {
@@ -27,3 +32,15 @@ afterAll(async () => {
     await mongo.stop()
     await mongoose.connection.close()
 })
+
+export const getCookieHandler = async () => {
+    const email = 'test@test.com'
+    const password = 'password'
+
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({ email, password })
+        .expect(201)
+
+    return response.get('Set-Cookie')
+}
